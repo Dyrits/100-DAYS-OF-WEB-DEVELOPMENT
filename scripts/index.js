655f941383dceb8bfaf147902d8956c433fb1ab0
Dyrits <dyrits@outlook.com>
2022-03-24 21:16:45 -0500

    Day #24-02 added.

--- scripts/index.js
+++
@@ -1,8 +0,0 @@
-window.onresize = () => {
-    window.matchMedia( "(min-width: 48rem)").matches && (window.location.hash = "");
-};
-
-document.querySelector("#menu-button").onclick = ($event) => {
-    $event.preventDefault();
-    window.location.hash = window.location.hash === "#drawer" ? "" : "#drawer";
-}

8ea78b8ac54de6927399a555dde018d415c9476f
Dyrits <dyrits@outlook.com>
2022-04-02 17:41:03 -0500

    Day #033 finished.

---
+++ scripts/index.js
@@ -0,0 +1,12 @@
+const { log, dir } = console;
+
+// Window
+log(window);
+log(window.alert === alert);
+
+// DOM
+log(document);
+dir(document);
+
+// { document: body : [main, ...] > [..., p, ...] > [a, ...] }
+document.body.children[0].children[1].children[0].href = "https://www.google.com";

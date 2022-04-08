b13ccf569e98d7a112fff07be904f121f6f4efa5
Dyrits <dyrits@outlook.com>
2022-04-07 19:19:51 -0500

    Refactoring with new class added.

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,12 @@
 const remainder = document.querySelector('#remainder');
+const methods = {
+    10: ({ classList }) => classList.add("warning"),
+    11: ({ classList }) => classList.remove("warning"),
+    0: ({ classList }) => classList.add("error"),
+    1: ({ classList }) => classList.remove("error")
+};
 
 document.querySelector("#name").oninput = function() {
-    const methods = { 10: "add", 11: "remove"};
-    const key = ( remainder.innerText = 60 - this.value.length );
-    [this, remainder].forEach(({ classList }) => classList[methods[key]]?.("warning"));
+    const key = (remainder.innerText = 60 - this.value.length);
+    key in methods && [this, remainder].forEach(methods[key]);
 };

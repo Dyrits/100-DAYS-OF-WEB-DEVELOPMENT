d5303b6ae1c2fdaab4b0350904cd0bf9dff316be
Dyrits <dyrits@outlook.com>
2022-04-07 19:48:08 -0500

    Code has been refactored.

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,7 @@
 const remainder = document.querySelector('#remainder');
 
 document.querySelector("#name").oninput = function() {
-  [this, remainder].forEach(element => element.classList[{ 10: "add", 11: "remove"}[remainder.innerText = 60 - this.value.length]]?.("warning"));
+  const actions = { 10: "add", 11: "remove"};
+  const key = (remainder.innerText = 60 - this.value.length);
+  key in actions && [this, remainder].forEach(element => element.classList[actions[key]]?.("warning"));
 };

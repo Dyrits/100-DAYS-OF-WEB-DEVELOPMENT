cf7ce7f62a31c50bc9edc2a4f4587edd2485a900
Dyrits <dyrits@outlook.com>
2022-04-07 19:00:22 -0500

    Code has been made easier to read.

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 const remainder = document.querySelector('#remainder');
 
 document.querySelector("#name").oninput = function() {
-    const method = { 10: "add", 11: "remove"};
-    remainder.innerText = 60 - this.value.length;
-    [this, remainder].forEach(({ classList }) => classList[method[remainder.innerText]]?.("warning"));
+    const methods = { 10: "add", 11: "remove"};
+    const key = remainder.innerText = 60 - this.value.length;
+    [this, remainder].forEach(({ classList }) => classList[methods[key]]?.("warning"));
 };

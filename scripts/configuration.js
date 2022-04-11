898c82081e94e39e893870cc76c0a42b57171682
Dyrits <dyrits@outlook.com>
2022-04-10 22:44:55 -0500

    Players object modified.

--- scripts/configuration.js
+++ scripts/configuration.js
@@ -1,9 +1,9 @@
 const players = {
-    "#1": { name: localStorage.getItem("#1") || "(Unknown) #1", icon: "X", nextPlayer: "#2" },
-    "#2": { name: localStorage.getItem("#2") || "(Unknown) #2", icon: "O", nextPlayer: "#1" },
+    "#1": { name: localStorage.getItem("#1") || "(Unknown) #1", icon: "X", _next: "#2" },
+    "#2": { name: localStorage.getItem("#2") || "(Unknown) #2", icon: "O", _next: "#1" },
     _current: "#1",
     get current() { return this[this._current];},
-    switchPlayer () { this._current = this.current.nextPlayer;},
+    switchPlayer () { this._current = this.current._next;},
 }
 
 function openModal() {

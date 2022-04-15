1eba942ea7653d2a5c58b51b8aee733c4d37260a
Dyrits <dyrits@outlook.com>
2022-04-14 21:28:47 -0500

    {once : true} added to the events.

--- scripts/functions/game.js
+++ scripts/functions/game.js
@@ -10,7 +10,7 @@ function resetGame() {
     game.round = 1;
     document.querySelectorAll("#game-board li").forEach(square => {
         square.hasChildNodes() && square.removeChild(square.firstChild);
-        square.addEventListener("click", placeIcon);
+        square.addEventListener("click", placeIcon, { once: true });
     });
     ["draw", "win"].forEach(status => { document.querySelector(`#result-${status}`).style.display = "none"; });
 }

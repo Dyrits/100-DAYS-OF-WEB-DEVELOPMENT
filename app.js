c395dfea6c4e2a908ad72f1e80990e133d728505
Dyrits <dyrits@outlook.com>
2022-05-05 18:14:04 -0500

    Minor update.

--- app.js
+++ app.js
@@ -25,6 +25,6 @@ app.use( (error, request, response, next) => {
 
 database.connect().then(() => {
   database.schema && app.listen(process.env.PORT || 3000, () => {
-    console.info("The server started on port 3000.");
+    console.info(`The server started on port ${process.env.PORT || 3000}.`);
   });
 });

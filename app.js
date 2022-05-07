3abc9dd377eaf54f3860b1f17bddad887355dbfa
Dyrits <dyrits@outlook.com>
2022-05-07 12:17:28 -0500

    Day #070-02 created.

--- app.js
+++ app.js
@@ -1,31 +1,24 @@
-const path = require("path");
-const express = require("express");
+const path = require('path');
 
-const database = require("./data/database");
+const express = require('express');
 
-const routes = {
-  blog: require("./routes/blog")
-}
+const db = require('./data/database');
+const demoRoutes = require('./routes/demo');
 
 const app = express();
 
-// Activate EJS view engine
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.urlencoded({ extended: true })); // Parse incoming request bodies
-app.use(express.json()); // Parse incoming request bodies
-app.use(express.static("public")); // Serve static files (e.g. CSS files)
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
 
-app.use(routes.blog);
+app.use(demoRoutes);
 
-app.use( (error, request, response, next) => {
-  console.log(error);
-  response.status(500).render("500");
-});
+app.use(function(error, req, res, next) {
+  res.render('500');
+})
 
-database.connect().then(() => {
-  database.schema && app.listen(process.env.PORT || 3000, () => {
-    console.info(`The server started on port ${process.env.PORT || 3000}.`);
-  });
+db.connectToDatabase().then(function () {
+  app.listen(3000);
 });

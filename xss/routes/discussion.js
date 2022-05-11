a0017a472fe81fa2b42e3e2a5179cd322235cc53
Dyrits <dyrits@outlook.com>
2022-05-11 16:18:05 -0500

    Day #074 finished.

--- xss/routes/discussion.js
+++ xss/routes/discussion.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const xss = require('xss');
 
 const db = require('../data/database');
 
@@ -15,7 +16,7 @@ router.get('/discussion', async function (req, res) {
 
 router.post('/discussion/comment', async function (req, res) {
   const comment = {
-    text: req.body.comment,
+    text: xss(req.body.comment),
   };
 
   await db.getDb().collection('comments').insertOne(comment);
